ef7bd01533e276c5c9345ec0b30bec5ae2e80993
heartmarshall <heartmarshall@yandex.ru>
2023-03-16 13:38:21 +0300

    Убрал возможность последовательного скачивания
    Пока что нет времени дописать

--- lab_03/client/tcp_client.py
+++ lab_03/client/tcp_client.py
@@ -14,15 +14,19 @@ LOCAL_STORAGE_PATH = os.path.join(os.getcwd(), 'localfiles')
 @click.option('-d', 'download_dir', default="")
 def run_client(file_name, ip, port, download_dir):
     client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_sock.connect((ip, port))
     rid = 0
+    client_sock.connect((ip, port))
+
     while True:
         save_file_from_server(client_sock, file_name, download_dir, rid)
-        print("Enter new file name or exit() to stop client") # Не получается скачать новый файл - выдаёт ошибку
-        file_name = input()
-        if file_name == "exit()":
-            exit()
-        rid += 1
+        exit()
+        # TODO: сделать так, чтобы можно было отправлять несколько запросов подряд
+        # print("Enter new file name or exit() to stop client...") # Не получается скачать новый файл - выдаёт ошибку
+        # file_name = input()
+        # if file_name == "exit()":
+        #     client_sock.close()
+        #     exit()
+        # rid += 1
 
 
 def send_request(client_sock, request):
@@ -33,9 +37,9 @@ def read_response(client_sock):
     response = bytearray()
     try:
         while True:
-            chunk = client_sock.recv(4)
+            chunk = client_sock.recv(1024)
             response += chunk
-            if len(chunk) < 4:
+            if len(chunk) < 1024:
                 return response
     except ConnectionResetError:
         # Соединение было неожиданно разорвано.
@@ -47,10 +51,16 @@ def save_file_from_server(client_sock, file_name, download_dir="", rid=None):
     send_request(client_sock, request)
     print(f"Request #{rid} sended")
     response_headers, response_body = read_response(client_sock).split(b'\r\n\r\n', 1)
+
+    if response_headers.split(b' ')[1] == b'404':
+        print("File not founded")
+        return 1
+    
     if not download_dir:
         download_dir = os.getcwd()
     with open(os.path.join(download_dir, file_name), "wb") as f:
         f.write(response_body)
+        print(f"File from request #{rid} saved!")
 
 
 if __name__ == '__main__':
