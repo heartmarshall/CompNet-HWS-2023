8b74f8dd82bf2e54bbb184cb1a99ec91b4c5bb0b
heartmarshall <heartmarshall@yandex.ru>
2023-03-16 12:29:37 +0300

    исправил ошибку со скачиванием картинок

--- lab_03/client/tcp_client.py
+++ lab_03/client/tcp_client.py
@@ -18,7 +18,7 @@ def run_client(file_name, ip, port, download_dir):
     rid = 0
     while True:
         save_file_from_server(client_sock, file_name, download_dir, rid)
-        print("Enter new file name or exit() to stop client")
+        print("Enter new file name or exit() to stop client") # Не получается скачать новый файл - выдаёт ошибку
         file_name = input()
         if file_name == "exit()":
             exit()
@@ -36,7 +36,7 @@ def read_response(client_sock):
             chunk = client_sock.recv(4)
             response += chunk
             if len(chunk) < 4:
-                return response.decode()
+                return response
     except ConnectionResetError:
         # Соединение было неожиданно разорвано.
         return None
@@ -46,11 +46,11 @@ def save_file_from_server(client_sock, file_name, download_dir="", rid=None):
     request = f"GET /{file_name} HTTP/1.1\r\n"
     send_request(client_sock, request)
     print(f"Request #{rid} sended")
-    response_headers, response_body = read_response(client_sock).split('\r\n\r\n', 1)
+    response_headers, response_body = read_response(client_sock).split(b'\r\n\r\n', 1)
     if not download_dir:
         download_dir = os.getcwd()
     with open(os.path.join(download_dir, file_name), "wb") as f:
-        f.write(response_body.encode())
+        f.write(response_body)
 
 
 if __name__ == '__main__':
